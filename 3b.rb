ceec8152597d4aafdcaeefb9548f51ad1fee86ed
Jeremy Stanley <jeremy@instructure.com>
2024-12-03 12:37:23 -0700

    why not just

--- 3b.rb
+++ 3b.rb
@@ -1,6 +1,1 @@
-def count(segment)
-  segment.scan(/mul\((\d{1,3}),(\d{1,3})\)/).sum { _1.map(&:to_i).inject(:*) }
-end
-
-chunks = ARGF.read.split("don't()")
-puts count(chunks.shift) + chunks.sum { |chunk| count(chunk.split("do()", 2)[1] || "") }
+puts ARGF.read.split("do()").sum { |chunk| chunk.split("don't()", 2).first.scan(/mul\((\d+),(\d+)\)/).sum { _1.map(&:to_i).inject(:*) } }
