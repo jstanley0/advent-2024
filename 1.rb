b38143b985252a472ad675b9bc9357afd1a56653
Jeremy Stanley <jeremy@instructure.com>
2024-12-10 11:54:59 -0700

    transpose/zip were made for this

--- 1.rb
+++ 1.rb
@@ -1,5 +1,3 @@
-data = ARGF.readlines.map { _1.split.map(&:to_i) }
-a = data.map(&:first).sort
-b = data.map(&:last).sort
-puts a.each_index.sum { |i| (b[i] - a[i]).abs }
+a, b = ARGF.readlines.map { _1.split.map(&:to_i) }.transpose.map(&:sort)
+puts a.zip(b).sum { (_2 - _1).abs }
 puts a.sum { b.count(_1) * _1 }
