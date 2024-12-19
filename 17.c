68c1eace2b87b4c30861db3f642623c74dbb5726
Jeremy Stanley <jeremy@instructure.com>
2024-12-18 22:57:52 -0700

    micro-optimize the chronospatial computer
    now it could brute-force the answer in three weeks
    instead of a month
    
    unfortunately or fortunately, we haven't seen
    the last of this dead horse

--- 17.c
+++ 17.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-const int quine[16] = {2,4,1,1,7,5,4,6,0,3,1,4,5,5,3,0};
-
 bool run(long a) {
-  int res[16];
-  size_t sz = 0;
-  long b;
-  while (a != 0 && sz < 16) {
+  int n = 0;
+  long b, r = 0;
+  while (a != 0 && n++ < 16) {
     b = (a & 7) ^ 1;
     b ^= (a >> b) ^ 4;
     a >>= 3;
-    res[sz++] = b & 7;
+    r = (r << 3) | b & 7;
   }
-  if (a != 0 || sz != 16)
-    return false;
-
-  for(size_t i = 0; i < 16; ++i)
-    if(res[i] != quine[i])
-      return false;
-
-  return true;
+  return a == 0 && n == 16 && r == 02411754603145530L;
 }
 
 int main()
